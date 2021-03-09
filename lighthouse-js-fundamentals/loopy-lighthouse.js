eed0a6a5af47144d3af9a7d725dc95d7c31a528e
Austin Kretzschmar <austinkret@gmail.com>
2021-03-09 23:01:36 +0000

    loopy-lighthouse.js for the looping coding

---
+++ lighthouse-js-fundamentals/loopy-lighthouse.js
@@ -0,0 +1,19 @@
+// 100 - 200
+// multiples of 3 - 'Loopy'
+// multiples of 4 - 'Lighthouse'
+// if both - 'LoopyLighthouse'
+// print console.log
+
+let num = 100;
+
+for (let num = 100; num <= 200; num++) {
+  if (num % 3 === 0 && num % 4 === 0) {
+    console.log("LoopyLighthouse");
+  } else if (num % 3 === 0) {
+    console.log("Loopy");
+  } else if (num % 4 === 0) {
+    console.log("Lighthouse");
+  } else {
+    console.log(num);
+  }
+}
